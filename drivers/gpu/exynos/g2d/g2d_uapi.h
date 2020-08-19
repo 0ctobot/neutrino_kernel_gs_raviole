MotorolaMobilityLLC/kernel-slsi

[COMMON] g2d: fix formula based on measured value
H/W performance varies depending on scaling ratio,
size, rotation and format. Based on the measured
times, PPC is calculated again.

Change-Id: I6fcfef745ec83eda26647a5d4bad9c994cff4a3f
Signed-off-by: hyesoo.yu <hyesoo.yu@samsung.com>

drivers/gpu/exynos/g2d/g2d_uapi.h
@@ -212,15 +212,23 @@ struct g2d_task_data {
 /* flags of g2d_performance_layer_data.layer_attr */
 #define G2D_PERF_LAYER_ROTATE		(1 << 0)
 #define G2D_PERF_LAYER_SCALING		(1 << 1)
-#define G2D_PERF_LAYER_YUV2P		(1 << 2)
+#define G2D_PERF_LAYER_YUV2P		(1 << 4)
+#define G2D_PERF_LAYER_YUV2P_82	(1 << 5)
+#define G2D_PERF_LAYER_FMTMASK		(3 << 4)
 
 /*
  * struct g2d_performance_frame_data - description of needed performance.
- * @pixelcount : the pixecount of layer, is used to calculate the frequency.
+ * @crop_width/height : the width and height of the image that
+ * 		participates in the image processing
+ * @window_width/height : the width and height of valid rectangle,
+ *		the region is drawn on target
  * @layer_attr : attribute of layer affecting performance.
  */
 struct g2d_performance_layer_data {
-	__u32 pixelcount;
+	__u16 crop_width;
+	__u16 crop_height;
+	__u16 window_width;
+	__u16 window_height;
 	__u32 layer_attr;
 };
 

0ctobot/neutrino_kernel_gs_raviole

Change-Id: I8993e20aee1c94f5a07290fe754e612c66adc8da
(cherry picked from commit ebd6d80644bc7d138ac8627550f1b521b5f1c023)
Signed-off-by: Mark Salyzyn <salyzyn@google.com>
Bug: 165562344

drivers/gpu/exynos/g2d/g2d_uapi.h
@@ -212,15 +212,23 @@ struct g2d_task_data {
 /* flags of g2d_performance_layer_data.layer_attr */
 #define G2D_PERF_LAYER_ROTATE		(1 << 0)
 #define G2D_PERF_LAYER_SCALING		(1 << 1)
-#define G2D_PERF_LAYER_YUV2P		(1 << 2)
+#define G2D_PERF_LAYER_YUV2P		(1 << 4)
+#define G2D_PERF_LAYER_YUV2P_82		(1 << 5)
+#define G2D_PERF_LAYER_FMTMASK		(3 << 4)
 
 /*
  * struct g2d_performance_frame_data - description of needed performance.
- * @pixelcount : the pixecount of layer, is used to calculate the frequency.
+ * @crop_width/height : the width and height of the image that
+ *		participates in the image processing
+ * @window_width/height : the width and height of valid rectangle,
+ *		the region is drawn on target
  * @layer_attr : attribute of layer affecting performance.
  */
 struct g2d_performance_layer_data {
-	__u32 pixelcount;
+	__u16 crop_w;
+	__u16 crop_h;
+	__u16 window_w;
+	__u16 window_h;
 	__u32 layer_attr;
 };
 