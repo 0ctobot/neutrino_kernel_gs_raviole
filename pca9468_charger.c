e586decac89d9dc9f1e9067a0b1b98ccd8b4c31a
Wasb Liu <wasbliu@google.com>
2021-09-17 15:41:13 +0800

    pca9468_charger: ta_vol adjust stage stop increasing power when hit VFLT_LOOP
    stop increasing the input power when iin < iin_cc and status hit VFLT_LOOP
    The device entered CV mode, so the iin could not increase
    during the adjustment.
    Stop increasing voltage to prevent requesting
    too much power and causing thermal concern.
    
    Bug: 199239859
    Signed-off-by: Wasb Liu <wasbliu@google.com>
    Change-Id: I6a770364cfe2d48eb71d2484e484da721f608bb2

--- pca9468_charger.c
+++ pca9468_charger.c
@@ -2101,7 +2101,16 @@ static int pca9468_adjust_ta_voltage(struct pca9468_charger *pca9468)
 	} else if (iin < (pca9468->iin_cc - PD_MSG_TA_CUR_STEP)) {
 		/* TA current is lower than the target input current */
 
-		if (pca9468->ta_vol == pca9468->ta_max_vol) {
+		if (pca9468_check_status(pca9468) == STS_MODE_VFLT_LOOP) {
+			/* IIN current may not able to increase in CV */
+
+			logbuffer_prlog(pca9468, LOGLEVEL_DEBUG,
+					"End1-1, skip adjust for cv, ta_cur=%u, ta_vol=%u, iin_cc=%u, chg_mode=%u",
+					pca9468->ta_cur, pca9468->ta_vol,
+					pca9468->iin_cc, pca9468->chg_mode);
+
+			pca9468_return_to_loop(pca9468);
+		} else if (pca9468->ta_vol == pca9468->ta_max_vol) {
 			/* TA TA voltage is already at the maximum voltage */
 
 			logbuffer_prlog(pca9468, LOGLEVEL_DEBUG,
