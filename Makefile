e333977ef1ca98f0d30d26112187cc863f1ae4cc
Will McVicker <willmcvicker@google.com>
2021-11-05 13:52:13 -0700

    pcie: exynos-gs: enable support to compile this driver
    Updates the Makefiles and Kconfigs to allow compiling the Exynos GS PCIe
    driver. It also renames CONFIG_PCI_EXYNOS to CONFIG_PCI_EXYNOS_GS and
    the module name to pci-exynos-gs.ko to avoid conflicts with upstream.
    
    Bug: 205332698
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Change-Id: If0a770f9b0499a1a8dcf9ad7a03e2b38ab46a5d8

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 # SPDX-License-Identifier: GPL-2.0
 
+obj-y += drivers/pci/controller/dwc/
+
 obj-y += drivers/scsi/ufs/
 
--- include/linux/exynos-pci-ctrl.h
+++ include/linux/exynos-pci-ctrl.h
@@ -17,7 +17,7 @@
 #define PCIE_L1SS_CTRL_WIFI	(0x1 << 4)
 #define PCIE_L1SS_CTRL_TEST	(0x1 << 31)
 
-#if IS_ENABLED(CONFIG_PCI_EXYNOS)
+#if IS_ENABLED(CONFIG_PCI_EXYNOS_GS)
 extern int exynos_pcie_l1ss_ctrl(int enable, int id);
 extern int exynos_pcie_rc_l1ss_ctrl(int enable, int id, int ch_num);
 #endif
